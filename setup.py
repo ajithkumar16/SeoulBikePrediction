8b40e65f47d2708b5095674c209c45a5a4d20646
ajithkumar <ajithkumar16796@gmail.com>
2022-07-05 16:31:29 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mlops pset 2',
+    author='Ajith Kumar K',
+    license='',
+)
